273f7c45b37bf2b77b1949ee61f9cb874bbf627f
m.sadofyev <m.sadofyev@infinnity.ru>
2016-11-02 10:36:06 +0500

    Убрана инфорамция о Sphinx из footer'а

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -207,7 +207,7 @@ html_context = {
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #
-# html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #
